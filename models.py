5cdb48ae9bc0c1329ec269dc2450098ee22c3320
Bogdankozlovskiy <bkozlovsky@bk.ru>
2020-10-02 17:56:13 +0300

    Update models.py

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=50)
     text = models.TextField(verbose_name="текст")
-    rate = models.ManyToManyField(User, through="managebook.BookRate", related_name="user")
+    rate = models.ManyToManyField(User, through="managebook.BookRate", related_name="rated_book")
     cached_rate = models.DecimalField(max_digits=3, decimal_places=2, default=0.0, verbose_name="рэйтинг")
 
 
@@ -13,24 +13,24 @@ class Book(models.Model):
 class Comment(models.Model):
     text = models.TextField(verbose_name="текст")
     date = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="пользователь", related_name="comment")
-    book = models.ForeignKey(Book, on_delete=models.CASCADE, verbose_name="Книга", related_name="comment")
-    like = models.ManyToManyField(User, through='managebook.CommentLike', related_name="liked_comment")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="пользователь", related_name="comments")
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, verbose_name="Книга", related_name="comments")
+    like = models.ManyToManyField(User, through='CommentUser', related_name="liked_comment")
     cached_likes = models.PositiveIntegerField(default=0)
     
 
 
-class BookRate(models.Model):
+class BookUser(models.Model):
     class Meta:
         unique_together = ("user", 'book')
 
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="book_like")
-    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="book_like")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="book_user")
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="book_user")
     rate = models.PositiveIntegerField(default=0)
 
 
 
-class CommentLike(models.Model):
+class CommentUser(models.Model):
     class Meta:
         unique_together = ("user", 'comment')
 
